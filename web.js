107592b302490f00cce62605f5d8546dbf5c17e1
sevenflash <sevenflash@yandex.ru>
2018-08-07 12:25:49 +0300

    deploy

--- web.js
+++ web.js
@@ -17,7 +17,6 @@ var server = app.listen(process.env.PORT, "0.0.0.0", () => {
 
 module.exports = (bot) => {
 	app.post('/' + bot.token, (req, res) => {
-		bot.processUpdate(req.body);
 		res.sendStatus(200);
 	});
 };
